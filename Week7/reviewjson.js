f8241b735e314a8844503de7082b3f057e1e62a3
meen116 <116251959+meen116@users.noreply.github.com>
2022-12-01 15:06:08 +0700

    bookstore

---
+++ Week7/reviewjson.js
@@ -0,0 +1,8 @@
+let obj = {
+    FirstName : "Jack",
+    LastName : "Smith",
+    Age : 25
+};
+let myJSON = '{"FirstName" : "Sam","LastName" : "Smith","age"}'
+console.log("myJSON",typeof(myJSON));
+
---
+++ Week7/reviewjson3.js
@@ -0,0 +1,8 @@
+let obj = {
+    FirstName : "Jack",
+    LastName : "Smith",
+    Age : 25
+};
+
+let myJSON = JSON.stringify(obj);
+console.log(myJSON,typeof(myJSON));
